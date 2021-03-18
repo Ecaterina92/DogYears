0809abd588d8acc549ed58ad835c12870e8c3425
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-03-18 17:15:25 +0200

    First Commit

---
+++ dog.js
@@ -0,0 +1,16 @@
+let myAge = 28; // Introduced a variable with my age value
+let earlyYears = 2; //Introduced a variable early years
+
+earlyYears *= 10.5;
+
+
+let laterYears = myAge - 2; // Introduced a new variable laterYears
+
+laterYears *= 4;
+
+let myAgeInDogYears = laterYears + earlyYears; // Added a new variable with value of my age in dog years
+
+
+const myName = 'Ecaterina'.toLowerCase(); // My name lowercased
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
